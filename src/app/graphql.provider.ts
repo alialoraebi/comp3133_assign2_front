ab92e5fe52095b1d4ce49e4e8f92e38da955de17
ali <a.aloreabi2000@gmail.com>
2024-04-07 01:10:51 -0400

    edited uri

--- src/app/graphql.provider.ts
+++ src/app/graphql.provider.ts
@@ -4,7 +4,7 @@ import { ApplicationConfig, inject } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 
 // export const uri = 'http://localhost:4000/graphql'; 
-export const uri = 'https://comp3133-assignment2-back.onrender.com';
+export const uri = 'https://comp3133-assignment2-back.onrender.com/graphql';
 export function apolloOptionsFactory(): ApolloClientOptions<any> {
   const httpLink = inject(HttpLink);
   return {
